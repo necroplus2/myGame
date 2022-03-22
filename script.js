3b2e1f34a15e535849a98cd247e9e3535e8cdcff
necroplus2 <99741579+necroplus2@users.noreply.github.com>
2022-03-22 17:19:36 +0700

    sudoku v.1

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // const main = document.querySelector('.main');
 // const diagonal = document.querySelector('.diagonal');
-// //console.log(diagonal);
+// console.log(diagonal);
 
 // main
 const main = document.createElement("div");
@@ -10,8 +10,13 @@ main.classList.add("main");
 const game = document.createElement("div");
 game.classList.add("game");
 
+// menu
+const menu = document.createElement("div");
+menu.classList.add("menu");
+
 // appendChild after head
 document.body.appendChild(main).appendChild(game);
+main.appendChild(menu);
 
 // diagonal
 const diagonal = document.createElement("div");
@@ -25,7 +30,48 @@ for(let i = 0; i < 9; i++) {
   kotak.classList.add("kotak");
   for(let i = 0; i < 9; i++) {
     const kotakAkm = kotak.cloneNode(true);
-    kotakAkm.innerHTML = i+1;
+    // kotakAkm.innerHTML = i+1;
     diagonalAkm.appendChild(kotakAkm);
   }
 }
+
+
+const kotakIsi = document.querySelectorAll('.kotak');
+
+kotakIsi.forEach(function(el){
+  el.addEventListener('click', function(e){
+    // console.log(e.target.parentElement);
+    e.target.classList.add('kotakKlik');
+    e.target.parentElement.classList.add('diagonalKlik');
+    if(e.target !== e.target) {
+      alert('yes');
+    }
+
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
