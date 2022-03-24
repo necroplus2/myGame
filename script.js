30c856fc498d90a17a7b4bf2ede6a7cd9c29d3c6
necroplus2 <99741579+necroplus2@users.noreply.github.com>
2022-03-24 17:07:37 +0700

    v.0.1

--- script.js
+++ script.js
@@ -41,20 +41,64 @@ const kotakIsi = document.querySelectorAll('.kotak');
 kotakIsi.forEach(function(el){
   el.addEventListener('click', function(e){
     // console.log(e.target.parentElement);
+
     e.target.classList.add('kotakKlik');
     e.target.parentElement.classList.add('diagonalKlik');
-    if(e.target !== e.target) {
-      alert('yes');
+    // const diagonalKlik = e.target.classList[1];
+/*
+    // cek yg di klik(e.target) sama atau tidak dengan anak pertama(firstChild) dari parent(parentNode) yg di klik
+    if(e.target == e.target.parentNode.firstChild){
+      // console.log("yes");
+      // looping angka sampai 8 karena dikurangi 1(firstChild)
+      for(let i = 0; i < 8; i++){
+        // console.log(Array.from(e.target.parentNode.childNodes[i+1].classList)[1]);
+        if(Array.from(e.target.parentNode.childNodes[i+1].classList)[1] == "kotakKlik"){
+          e.target.parentNode.childNodes[i+1].classList.remove("kotakKlik");
+        }
+      }
     }
+*/
+
+    // console.log(e);
+    for(let i = 0; i < 9; i++){
+      if(e.target == e.target.parentNode.childNodes[i]){
+        // console.log(e);
+        
+        for(let j = 0; j < 9; j++){
+
+          // buat firstChild
+          // console.log(i+j+1);
+          // console.log(e.target.parentNode.childNodes[i+j+1]);
+          // buat lastChild
+          // console.log(i-j-1);
+          // console.log(e.target.parentNode.childNodes[i-j-1]);
+
+          console.log(i+j);
+
+          // if(e.target == e.target.parentNode.firstChild){
+          //   e.target.nextElementSibling.classList.remove("kotakKlik");  
+          // }else if(Array.from(e.target.parentNode.childNodes[i-j-1].classList)[1] == "kotakKlik"){
+          //   console.log(i-j-1);
+          //   e.target.parentNode.childNodes[i-j-1].classList.remove("kotakKlik");
+          // }else if(Array.from(e.target.parentNode.childNodes[i+j+1].classList)[1] == "kotakKlik"){
+          //   console.log(i+j+1);
+          //   e.target.parentNode.childNodes[i+j+1].classList.remove("kotakKlik");
+          // }
+
+
+
+        }
+
+      }
+    }
+
+
+
+
+
 
   });
 });
-
-
-
-
-
-
 
 
 
