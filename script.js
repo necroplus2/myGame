56b9f19f06b837f01cb4c1cd48e8b9794f0ba171
necroplus2 <99741579+necroplus2@users.noreply.github.com>
2022-03-27 22:24:57 +0700

    v.0.3

--- script.js
+++ script.js
@@ -42,26 +42,34 @@ kotakIsi.forEach(function(el){
   el.addEventListener('click', function(e){
     // console.log(e.target.parentElement);
 
-    
-
+    const classKotak = document.querySelectorAll('.kotak');
 
     // e.target.classList.add('kotakKlik');
     // e.target.parentElement.classList.add('diagonalKlik');
 
+    // looping i lebih kecil dari 9 berfungsi untuk memanggil iterasi dari array parentNode.childNodes[(dari 0 - 8)]
     for(let i = 0; i < 9; i++){
 
-
-
+      // jika, parent yg di klik sama dengan anak dari grandparent yg di iterasi dan parent yg di klik memiliki class kedua yg nilainya "diagonalKlik" maka, jalankan add atau tambah class diagonalKlik di parent yg di klik
       if(e.target.parentNode == e.target.parentNode.parentNode.childNodes[i] && e.target.parentNode.classList[1] == "diagonalKlik"){
         e.target.parentNode.classList.add("diagonalKlik");
+
+      // jika tidak, anak anak dari grandparent ada yg memiliki class kedua "diagonalKlik" maka, jalankan hapus class yg classnya bernama "diagonalKlik"
+      // lalu melakukan looping untuk mengecek anak anak dari tiap parent parent untuk melakukan kondisi
+      // jika, anak anak dari para parent ada yg memiliki class "kotakKlik" maka, jalankan hapus class yg classnya bernama "kotakKlik"
       }else if(e.target.parentNode.parentNode.childNodes[i].classList[1] == "diagonalKlik"){
         e.target.parentNode.parentNode.childNodes[i].classList.remove("diagonalKlik");
+        for(let j = 0; j < 81; j++){
+          if(classKotak[j].classList[1] == "kotakKlik" && e.target !== classKotak[j]){
+            classKotak[j].classList.remove("kotakKlik");
+          }
+        }
+      // jika tidak, yg di klik memiliki parent yg sama dari para parent maka, jalankan tambah class yg bernama "diagonalKlik"
       }else if(e.target.parentNode == e.target.parentNode.parentNode.childNodes[i]){
         e.target.parentNode.classList.add("diagonalKlik");
       }
       
-
-
+      
       if(e.target.parentNode == e.target.parentNode.parentNode.children[i] && e.target.parentNode.classList[1] == "diagonalKlik"){
         e.target.parentNode.classList.add("diagonalKlik");
       }
